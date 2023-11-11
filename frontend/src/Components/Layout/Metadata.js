a88035fa8eb87238cdcdff6d5669d175a3c0d5f7
Emmanuel-Mingala <emmanuel.mingala@tup.edu.ph>
2023-11-11 20:31:51 +0800

    Added title param

--- frontend/src/Components/Layout/Metadata.js
+++ frontend/src/Components/Layout/Metadata.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-const MetaData = () => {
+const MetaData = ({title}) => {
     return (
         <Helmet>
             <title>{`${title} - ShopIT`}</title>
